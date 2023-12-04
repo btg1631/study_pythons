c130c5f8b2fc92348f2dbc12211997c1572cd0e9
youngji Jang <btg1631@naver.com>
2023-12-04 12:49:13 +0900

    first commit

---
+++ docs/welcomes.py
@@ -0,0 +1,17 @@
+## 변수 선언 후 저의 시 고려점 (넣는 값이 문자 or 숫자)
+# 문자 출력
+helloworld = "Hello, World! Youngji Jang." # 문자형 변수
+print(helloworld)
+
+# 숫자 합산 출력
+numbers = 3 + 5 # 숫자형 변수
+print(numbers)
+
+# 데이터 타입: 문자형 or 숫자형 등 변수에 대한 정의
+
+
+# 가정문 
+if 5 > 2: # 묶은 기호인 :과 tab은 하나의 쌍
+    pass
+    print("Five is greater than two!")
+print("end")
