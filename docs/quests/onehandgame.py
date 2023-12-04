9b3689438af862694da58eeaa7f4ceafb5c13173
youngji Jang <btg1631@naver.com>
2023-12-04 16:17:43 +0900

    onehandgame

--- docs/quests/onehandgame.py
+++ docs/quests/onehandgame.py
@@ -25,3 +25,42 @@ print("{} {} {} {} {}".format(one, two, three, four, five))
 
 two, three = three, two
 print("{} {} {} {} {}".format(one, two, three, four, five))
+
+print("--------------------------------------")
+
+first = "1"
+second = " "
+third = "2"
+fourth = " "
+fifth = "3"
+print("{} {} {} {} {}".format(first, second, third, fourth, fifth))
+
+second = first
+first = " "
+print("{} {} {} {} {}".format(first, second, third, fourth, fifth))
+
+fourth = second
+second = " "
+print("{} {} {} {} {}".format(first, second, third, fourth, fifth))
+
+second = third
+third = " "
+print("{} {} {} {} {}".format(first, second, third, fourth, fifth))
+
+third = fifth
+fifth = " "
+print("{} {} {} {} {}".format(first, second, third, fourth, fifth))
+
+fifth = fourth
+fourth = " "
+print("{} {} {} {} {}".format(first, second, third, fourth, fifth))
+
+first = third 
+third = " "
+print("{} {} {} {} {}".format(first, second, third, fourth, fifth))
+
+third = second
+second = " " 
+print("{} {} {} {} {}".format(first, second, third, fourth, fifth))
+
+
