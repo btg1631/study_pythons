f7a4ea386f2b4c337dc941dc3f3424778847b45e
youngji Jang <btg1631@naver.com>
2023-12-05 16:28:20 +0900

    bmis

--- docs/quests/bmis.py
+++ docs/quests/bmis.py
@@ -4,12 +4,12 @@ height = int(input("키를 입력하세요. : "))
 BMI = weight/(height**2)
 
 if 25 <= BMI:
-    print("{}kg으로 비만입니다.".format(BMI))
+    print("{}으로 비만입니다.".format(BMI))
 elif 23 <= BMI <= 24:
-    print("{}kg으로 과체중입니다.".format(BMI))
+    print("{}으로 과체중입니다.".format(BMI))
 elif 18 <= BMI <= 22:
-    print("{}kg으로 정상입니다.".format(BMI))
+    print("{}으로 정상입니다.".format(BMI))
 else:
-    print("{}kg으로 저체중입니다.".format(BMI))
+    print("{}으로 저체중입니다.".format(BMI))
 
 
