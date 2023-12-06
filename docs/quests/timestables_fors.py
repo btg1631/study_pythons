1dab24141ce25ad6c380a73c34386d31503c99ea
youngji Jang <btg1631@naver.com>
2023-12-06 11:29:36 +0900

    times

--- docs/quests/timestables_fors.py
+++ docs/quests/timestables_fors.py
@@ -1,4 +1,4 @@
-num_first = int(input())
+num_first = int(input("구구단 몇 단을 출력할까요? : "))
 
 for num_second in range(1, 10):
     print("{} * {} = {}".format(num_first, num_second, num_first*num_second))
