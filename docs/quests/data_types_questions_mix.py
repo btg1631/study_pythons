fb2b46e7066b2c2f847fc64737225724a13c2fb1
youngji Jang <btg1631@naver.com>
2023-12-12 15:49:20 +0900

    questions mix

--- docs/quests/data_types_questions_mix.py
+++ docs/quests/data_types_questions_mix.py
@@ -36,20 +36,20 @@ def questions(num):
     list_answer_input = []
 
 
-    for _ in range(num):
-        question_input = input()
+    for num_count in range(num):
+        question_input = input("{}번 문제 입력: ".format(num_count+1))
         dict_question_mix["question"] = question_input
 
         for _ in range(4):
-            answer_input = input()
+            answer_input = input("답항 입력: ")
             list_answer_input.append(answer_input)
         dict_question_mix["answer"] = list_answer_input
         list_answer_input = []
 
-        correct_input = int(input())
+        correct_input = int(input("정답 입력: "))
         dict_question_mix["correct_index"] = correct_input 
 
-        score_input = int(input())
+        score_input = int(input("점수 입력: "))
         dict_question_mix["score"] = score_input
 
         mixed_questions.append(dict_question_mix)
