47567c60818a7444f2932987f5934e112ac053d1
youngji Jang <btg1631@naver.com>
2024-04-05 12:46:14 +0900

    new branch

---
+++ docs/temps.py
@@ -0,0 +1,14 @@
+temp_list = [1,2,3,4,5,6] # 1차원 행렬
+temp_list[0] = 1
+
+temp_array = [[1,2],[3,4],[5,6]] # 2차원 행렬
+# print(temp_array[0][0])
+
+# for i in temp_list:
+#     print(i)
+temp_list = ['asdf','xcvb'] # 1차원 행렬
+
+for i in temp_list:
+    for j in i:
+        print(j)
+    pass
